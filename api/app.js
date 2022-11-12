bac88d6551676014e247015a33512d0d0e4dd15b
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2022-11-12 14:16:11 +0700

    Add CORS allow all

--- api/app.js
+++ api/app.js
@@ -15,6 +15,12 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 // set the static files
 app.use(express.static(PATH));
 http.createServer(app).listen(PORT, () => {
