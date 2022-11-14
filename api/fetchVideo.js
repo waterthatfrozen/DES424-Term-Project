64196aa616e2dccf003848d963de3c8f36fd95e8
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2022-11-14 14:46:28 +0700

    DTP-36 add fetch video info with videoID

--- api/fetchVideo.js
+++ api/fetchVideo.js
@@ -11,13 +11,23 @@ module.exports = async function (req,res) {
     let db = await client.db(`qikvid-db`);
     let collection = await db.collection('videos');
 
-    // Get one random video
-    let allVideos = await collection.find({}).toArray();
-    let video = allVideos[Math.floor(Math.random()*allVideos.length)];
-    
-    // Add username
-    const foundUser = await db.collection('users').findOne({ _id: ObjectId(video.userID) });
-    video.username = foundUser.username;
-
-    res.status(200).send(video);
+    if(req.query.videoID){
+        let video = await collection.findOne({_id: ObjectId(req.query.videoID)});
+        if(!video){
+            res.status(400).send({errorName: "Bad Request", errorMessage: "No video asset found."});
+        }else{
+            // Add username
+            const foundUser = await db.collection('users').findOne({ _id: ObjectId(video.userID) });
+            video.username = foundUser.username;   
+            res.status(200).send(video);
+        }
+    }else{
+        // Get one random video
+        let allVideos = await collection.find({}).toArray();
+        let video = allVideos[Math.floor(Math.random()*allVideos.length)];   
+        // Add username
+        const foundUser = await db.collection('users').findOne({ _id: ObjectId(video.userID) });
+        video.username = foundUser.username;
+        res.status(200).send(video);
+    }
 }
